a5ff139639b2779ad0ea9dac1b52ecf84625cc1a
Mattlomet <mattlomet@live.com>
2019-02-27 20:43:39 -0500

    mobile dropdown

--- src/components/menu/menuNav/breakfastNav.js
+++ src/components/menu/menuNav/breakfastNav.js
@@ -150,9 +150,9 @@ if (document.getElementById("item1").value === "French Toast"){
         <nav class="tabs is-boxed is-fullwidth is-info">
             {
                 (isDesktop)
-                ? <div className="container  dropdown">
+                ? <div className="container">
                 
-                <select id="item1" onChange={this.menuSelectMobile} >
+                <select id="item1" className="is-size-5" onChange={this.menuSelectMobile} >
                     <option value="French Toast" >French Toast</option>
                     <option value="Waffles">Waffles</option>
                     <option value="Eggs">Eggs</option>
